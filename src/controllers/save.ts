b2d447d53f98beef5ea2808dcc4295ce476543f7
Sebastian Robayo <sebastianrobayog@gmail.com>
2023-04-15 20:06:55 -0500

    [Fix] Resivisón del enrrutado.

--- src/controllers/save.ts
+++ src/controllers/save.ts
@@ -1,13 +1,22 @@
-import {Request, Response} from 'express';
-import {saveEquipment} from '../services/equipment';
+import { Request, Response } from "express";
+import { saveEquipment } from "../services/equipment";
+import { response, responseError } from "../utils/response.handle";
+import responseApi from "../lang/response-api";
 
-const saveEquipmentCtrl = async ({body}: Request, res: Response) => {
-    try {
-        const response = await saveEquipment(body);
-        res.send(response);
-    } catch (error) {
-        console.log(error);
-    }
-}
+const saveEquipmentCtrl = async ({ body }: Request, res: Response) => {
+  try {
+    const response = await saveEquipment(body);
+    res.send(response);
+  } catch (e) {
+    return response(
+      responseError({
+        statusCode: 500,
+        message: responseApi.general.serverError,
+        data: e,
+      }),
+      res
+    );
+  }
+};
 
-export {saveEquipmentCtrl};
+export { saveEquipmentCtrl };
