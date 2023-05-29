28f2594e4df75ec9876543ff202cfbd670b05db3
leydijo <leydilenis85@gmail.com>
2023-05-29 11:06:42 -0500

    Configuracion con Heroku

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -13,7 +13,7 @@ readdirSync(PATH_ROUTER).filter((fileName) => {
   const cleanName = cleanFileName(fileName);
   if (cleanName !== "index") {
     import(`./${cleanName}`).then((moduleRouter) => {
-      // console.log(`Se esta cargando la ruta... /${cleanName}`);
+       //console.log(`Se esta cargando la ruta... /${cleanName}`);
       router.use(`/${cleanName}`, moduleRouter.router);
     });
   }
